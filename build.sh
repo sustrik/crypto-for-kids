25dba1fe15023fc10ddc49f5ef4bd559e816569a
Martin Sustrik <sustrik@250bpm.com>
2017-06-09 22:45:21 +0200

    Fix filename
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ nodejs build.js
 
 pandoc -f markdown_github body.md > body.tmp
 
-cat header.html body.tmp footer.html > index.hmtl
+cat header.html body.tmp footer.html > index.html
 
 rm body.tmp
 rm body.md
